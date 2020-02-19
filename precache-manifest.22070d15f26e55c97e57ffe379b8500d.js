407b9753cc61d1e014f19b80dfbde4bf4112d4d0
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-19 14:25:18 +0000

    Updates

---
+++ precache-manifest.22070d15f26e55c97e57ffe379b8500d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1ac75aafaff4c62c702ab7220ad6fe",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "9b380c4de1299c53d547",
+    "url": "/calculator/static/css/main.f5152187.chunk.css"
+  },
+  {
+    "revision": "fb47d7f831adc60454ad",
+    "url": "/calculator/static/js/2.8a130742.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.8a130742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b380c4de1299c53d547",
+    "url": "/calculator/static/js/main.73182f05.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
